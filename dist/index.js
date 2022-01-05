aa334e6a86e1a953916ccdc5a4a5ad72c5a0c799
linwenxiang <991154416@qq.com>
2022-01-05 22:37:42 +0800

    test

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,32 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 439:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getInclude = void 0;
+function getInclude(all, node) {
+    const include = [];
+    for (const key in all[node]) {
+        if (Object.prototype.hasOwnProperty.call(all, key)) {
+            const element = all[key];
+            if (Object.keys(element).length === 0) {
+                include.push(key);
+                continue;
+            }
+            include.push(...getInclude(all, key));
+        }
+    }
+    return include;
+}
+exports.getInclude = getInclude;
+
+
+/***/ }),
+
 /***/ 264:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -36,91 +62,88 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(737));
-// import {dict, getInclude} from './leaf'
-// import {existsSync, readFileSync, readdirSync, statSync} from 'fs'
-// import {join} from 'path'
+const leaf_1 = __nccwpck_require__(439);
+const fs_1 = __nccwpck_require__(147);
+const path_1 = __nccwpck_require__(17);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        core.setOutput('need_ci', 'true');
-        core.setOutput('leaf', `["service0", "service1", "service2"]`);
-        // try {
-        //   const changePaths: string = core.getInput('change-paths')
-        //   core.debug(`Chanages ${changePaths} `)
-        //   const ignorePaths: string = core.getInput('ignore-paths')
-        //   core.debug(`Ignores ${ignorePaths} `)
-        //   const workspace: string = core.getInput('workspace')
-        //   core.debug(`Workspace ${workspace}`)
-        //   const ignores = ignorePaths.split(',')
-        //   const dirs: string[] = []
-        //   for (const e of readdirSync(workspace)) {
-        //     if (statSync(join(workspace, e)).isDirectory() && !ignores.includes(e)) {
-        //       dirs.push(e)
-        //     }
-        //   }
-        //   const depsAll: dict = {}
-        //   for (const p of dirs) {
-        //     depsAll[p] = {}
-        //     for (const pwd of dirs) {
-        //       if (pwd === p) {
-        //         continue
-        //       }
-        //       const settingsGradle: string = join(workspace, pwd, 'settings.gradle')
-        //       if (!existsSync(settingsGradle)) {
-        //         continue
-        //       }
-        //       const lter = readFileSync(settingsGradle, {encoding: 'utf8'}).matchAll(
-        //         /includeBuild\s'([^']+)'/g
-        //       )
-        //       const arr = [...lter]
-        //       for (const match of arr) {
-        //         const pathInclude = match[0]
-        //         if (
-        //           pathInclude.split('/').length > 0 &&
-        //           pathInclude.split('/')[1] === pwd
-        //         ) {
-        //           depsAll[pwd][p] = {}
-        //         }
-        //       }
-        //       // while (!lter.next().done) {
-        //       //   core.debug(lter.next().value)
-        //       //   const pathInclude = lter.next().value[1]
-        //       //   if (pathInclude instanceof String) {
-        //       //     if (
-        //       //       pathInclude.split('/').length > 0 &&
-        //       //       pathInclude.split('/')[1] === pwd
-        //       //     ) {
-        //       //       depsAll[pwd][p] = {}
-        //       //     }
-        //       //   }
-        //       // }
-        //     }
-        //   }
-        //   const leaf: string[] = []
-        //   const includeNodes: string[] = []
-        //   for (const p of changePaths.split(',')) {
-        //     const a = p.split('/')[0].trim()
-        //     if (ignores.includes(a)) {
-        //       return
-        //     }
-        //     if (statSync(join(workspace, a)).isDirectory()) {
-        //       if (includeNodes.includes(a)) {
-        //         return
-        //       }
-        //       if (Object.keys(depsAll[a]).length === 0) {
-        //         leaf.push(a)
-        //       } else {
-        //         leaf.push(...getInclude(depsAll, a))
-        //       }
-        //     }
-        //   }
-        //   // leaf = Array.from(new Set(leaf))
-        //   // core.setOutput('need_ci', leaf.length > 0 ? 'true' : 'false')
-        //   // core.setOutput('leaf', JSON.stringify(leaf))
-        //   core.setOutput('need_ci', 'true')
-        //   core.setOutput('leaf', `["service0", "service1", "service2"]`)
-        // } catch (error) {
-        //   if (error instanceof Error) core.setFailed(error.message)
-        // }
+        try {
+            const changePaths = core.getInput('change-paths');
+            core.debug(`Chanages ${changePaths} `);
+            const ignorePaths = core.getInput('ignore-paths');
+            core.debug(`Ignores ${ignorePaths} `);
+            const workspace = core.getInput('workspace');
+            core.debug(`Workspace ${workspace}`);
+            const ignores = ignorePaths.split(',');
+            const dirs = [];
+            for (const e of (0, fs_1.readdirSync)(workspace)) {
+                if ((0, fs_1.statSync)((0, path_1.join)(workspace, e)).isDirectory() && !ignores.includes(e)) {
+                    dirs.push(e);
+                }
+            }
+            const depsAll = {};
+            for (const p of dirs) {
+                depsAll[p] = {};
+                for (const pwd of dirs) {
+                    if (pwd === p) {
+                        continue;
+                    }
+                    const settingsGradle = (0, path_1.join)(workspace, pwd, 'settings.gradle');
+                    if (!(0, fs_1.existsSync)(settingsGradle)) {
+                        continue;
+                    }
+                    const lter = (0, fs_1.readFileSync)(settingsGradle, { encoding: 'utf8' }).matchAll(/includeBuild\s'([^']+)'/g);
+                    const arr = [...lter];
+                    for (const match of arr) {
+                        const pathInclude = match[0];
+                        if (pathInclude.split('/').length > 0 &&
+                            pathInclude.split('/')[1] === pwd) {
+                            depsAll[pwd][p] = {};
+                        }
+                    }
+                    // while (!lter.next().done) {
+                    //   core.debug(lter.next().value)
+                    //   const pathInclude = lter.next().value[1]
+                    //   if (pathInclude instanceof String) {
+                    //     if (
+                    //       pathInclude.split('/').length > 0 &&
+                    //       pathInclude.split('/')[1] === pwd
+                    //     ) {
+                    //       depsAll[pwd][p] = {}
+                    //     }
+                    //   }
+                    // }
+                }
+            }
+            const leaf = [];
+            const includeNodes = [];
+            for (const p of changePaths.split(',')) {
+                const a = p.split('/')[0].trim();
+                if (ignores.includes(a)) {
+                    continue;
+                }
+                if ((0, fs_1.statSync)((0, path_1.join)(workspace, a)).isDirectory()) {
+                    if (includeNodes.includes(a)) {
+                        continue;
+                    }
+                    if (Object.keys(depsAll[a]).length === 0) {
+                        leaf.push(a);
+                    }
+                    else {
+                        leaf.push(...(0, leaf_1.getInclude)(depsAll, a));
+                    }
+                }
+            }
+            // leaf = Array.from(new Set(leaf))
+            // core.setOutput('need_ci', leaf.length > 0 ? 'true' : 'false')
+            // core.setOutput('leaf', JSON.stringify(leaf))
+            core.setOutput('need_ci', 'true');
+            core.setOutput('leaf', `["service0", "service1", "service2"]`);
+        }
+        catch (error) {
+            if (error instanceof Error)
+                core.setFailed(error.message);
+        }
     });
 }
 run();
